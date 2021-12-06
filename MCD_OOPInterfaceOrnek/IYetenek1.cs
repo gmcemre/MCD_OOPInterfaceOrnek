77a7201c5ee6857e47db6ecac917944d3bda7b49
emre gemici <gmcc.emree@gmail.com>
2021-12-06 17:11:13 +0300

    Add project files.

---
+++ MCD_OOPInterfaceOrnek/IYetenek1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_OOPInterfaceOrnek
+{
+    public interface IYetenek1
+    {
+        //İnterface'in kendi erişim belirleyicisi neyse içindeki elemanların hepsi aynı erişime sahiptir.
+        //Bu nedenle interface içindeki elemanlara erişim belirleyicisi yazılmaz.
+        //İnterfacelerin metotlarının gövdesi olmaz.
+
+        void X(); //gövdesi yok
+        void Y();
+        void Z();
+    }
+}
